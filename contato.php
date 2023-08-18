385892259eb8e355f572f9e23ba400774d59c904
lcscastro <alves_lucas.castro@hotmail.com>
2023-08-18 14:57:03 -0300

    1280 x 800 - Responsividade

--- contato.php
+++ contato.php
@@ -303,5 +303,7 @@ INCLUDE_ONCE './config.php';
       <script src="custom.js"></script>
       <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
       <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+      <script src="//tag.goadopt.io/injector.js?website_code=4b357630-3b6c-4f31-b5b7-9c7467e24575" 
+class="adopt-injector"></script>
     </body>
 </html>
